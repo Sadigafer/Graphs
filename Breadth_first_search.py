1635c0ac5ed6ee09bda0fe52b39a4b9881dde755
Aleksandr <sanya.050498@yandex.ru>
2022-05-30 10:45:57 +0300

    Graphs

---
+++ Breadth_first_search.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+
+# Breadth first search
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is а mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+
+# Example
+
+graph = {}
+
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+search('you')
